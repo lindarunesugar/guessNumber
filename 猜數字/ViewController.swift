16b77bec814ff7efb108bb2098c30d8905abfc4d
HUANG PI-LING <lindarunesugar@yahoo.com.tw>
2017-03-02 18:58:04 +0800

    Initial Commit

---
+++ 猜數字/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  猜數字
+//
+//  Created by Huang Pi-Ling on 2017/3/2.
+//  Copyright © 2017年 Huang Pi-Ling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
