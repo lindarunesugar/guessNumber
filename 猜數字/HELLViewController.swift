68da44dd33a4c13a4302b08de09560769492bc87
HUANG PI-LING <lindarunesugar@yahoo.com.tw>
2017-03-04 04:12:56 +0800

    猜錯時，上下限有問題，然後再玩一次還沒做

--- 猜數字/HELLViewController.swift
+++ 猜數字/HELLViewController.swift
@@ -13,14 +13,18 @@ class HELLViewController: UIViewController
     
     @IBOutlet weak var number: UITextField!   //輸入的數字
     @IBOutlet weak var hint: UILabel!    //提示訊息
-    @IBOutlet weak var guessTime: UILabel!   //剩餘猜測次數
+    @IBOutlet weak var guessTime: UILabel!   //剩餘猜測次數    還有6次機會
     
-    var random_number = Int(arc4random_uniform(100))   //隨機產生的數字
+    var random_number = 4
+    //var random_number = Int(arc4random_uniform(100))   //隨機產生的數字
     var chance_count = 6    //初始猜測次數
     
     
     @IBAction func send(_ sender: Any)    //按鈕的功能
     {
+        var upper = 100
+        var lower = 0
+        
         let controller_wrong =
             self.storyboard?.instantiateViewController(withIdentifier:
                 "Wrong")
@@ -28,28 +32,113 @@ class HELLViewController: UIViewController
             self.storyboard?.instantiateViewController(withIdentifier:
                 "Right")
         
-        //按下計算按鈕時收起鍵盤
+        //按下送信時收起鍵盤
         number.resignFirstResponder()
         
-        while chance_count > 0  //還可以猜
+        if chance_count > 0
         {
             if number.text! == ""    //沒有輸入值
             {
                 hint.text! = "不猜也無妨..."
                 chance_count -= 1
+                guessTime.text! = "還有\(chance_count)次機會"
+                if chance_count == 0
+                {
+                    //利用寫程式的方法把controller叫出來
+                    self.present(controller_wrong!, animated: true, completion:
+                        nil)
+                }
+            }
+            else  //有輸入數字
+            {
+                let input = Int(number.text!)  //先把輸入的數字存下來
+                
+                if input == random_number  //猜對
+                {
+                    //利用寫程式的方法把controller叫出來
+                    self.present(controller_right!, animated: true, completion:
+                        nil)
+                }
+                else if input! > 100
+                {
+                    hint.text! = "你應該要再看清楚遊戲規則..."
+                    chance_count -= 1
+                    guessTime.text! = "還有\(chance_count)次機會"
+                    if chance_count == 0
+                    {
+                        //利用寫程式的方法把controller叫出來
+                        self.present(controller_wrong!, animated: true, completion:
+                            nil)
+                    }
+                    
+                }
+                else if input! < random_number
+                {
+                    lower = input!+1
+                    hint.text! = "你應該在\(lower)~\(upper)內猜一個數字"
+                    chance_count -= 1
+                    guessTime.text! = "還有\(chance_count)次機會"
+                    if chance_count == 0
+                    {
+                        //利用寫程式的方法把controller叫出來
+                        self.present(controller_wrong!, animated: true, completion:
+                            nil)
+                    }
+
+                }
+                else if input! > random_number
+                {
+                    upper = input!-1
+                    hint.text! = "你應該在\(lower)~\(upper)內猜一個數字"
+                    chance_count -= 1
+                    guessTime.text! = "還有\(chance_count)次機會"
+                    if chance_count == 0
+                    {
+                        //利用寫程式的方法把controller叫出來
+                        self.present(controller_wrong!, animated: true, completion:
+                            nil)
+                    }
+                    
+                }
+            }
+
+        }
+        else
+        {
+            //利用寫程式的方法把controller叫出來
+            self.present(controller_wrong!, animated: true, completion:
+                nil)
+        }
+        
+        /*while chance_count > 0  //還可以猜
+        {
+            if number.text! == ""    //沒有輸入值
+            {
+                hint.text! = "不猜也無妨..."
+                chance_count -= 1
+                guessTime.text! = "還有\(chance_count)次機會"
             }
             else  //輸入數字
             {
-                var input = Int(number.text!)
+                let input = Int(number.text!)  //先把輸入的數字存下來
                 
+                if input == random_number  //猜對
+                {
+                    //利用寫程式的方法把controller叫出來
+                    self.present(controller_right!, animated: true, completion:
+                        nil)
+                    break
+                }
             }
 
         }
         if chance_count == 0  //沒有機會了
         {
+            //利用寫程式的方法把controller叫出來
             self.present(controller_wrong!, animated: true, completion:
                 nil)
         }
+        */
         
     }
     
@@ -64,7 +153,7 @@ class HELLViewController: UIViewController
         
     }
     
-    //按下計算按鈕時收起鍵盤
+    //按下送信時收起鍵盤
     func textFieldShouldReturn(_ textField: UITextField) -> Bool
     {
         number.resignFirstResponder()
@@ -77,8 +166,6 @@ class HELLViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
-        
         // Do any additional setup after loading the view.
     }
 
